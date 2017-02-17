f96647c7a4440abd17a02267ae013c83e454269b
wojciechsmol <wojciechsmolcontact@gmail.com>
2017-02-17 12:29:10 +0100

    Design completed

--- app/src/main/java/com/example/android/footballcounter/MainActivity.java
+++ app/src/main/java/com/example/android/footballcounter/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.android.footballcounter;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Window;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
 }
